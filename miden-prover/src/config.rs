303f7452ff529e541cbf811d7269e1a49bf7522a
zhenfeizhang <zhenfei.zhang@hotmail.com>
2025-11-25 16:37:16 -0500

    impl prover

--- miden-prover/src/config.rs
+++ miden-prover/src/config.rs
@@ -1,6 +1,7 @@
 //! Re-export commonly used types from p3-uni-stark for easier access.
 
 pub use p3_uni_stark::AuxBuilder;
+pub use p3_uni_stark::Domain;
 pub use p3_uni_stark::PackedChallenge;
 pub use p3_uni_stark::PackedVal;
 pub use p3_uni_stark::PcsError;
